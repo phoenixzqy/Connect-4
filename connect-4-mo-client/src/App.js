ed737caa505966a2571f4bbf8fcab5bb07a17095
Felix Zhao <qzhao8731@gmail.com>
2019-12-17 15:47:23 -0800

    intergrate socket.io chatroom example into both client and server

--- connect-4-mo-client/src/App.js
+++ connect-4-mo-client/src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
 
 function App() {
+	const [socket, setSocket] = useState(null)
+	const [message, setMessage] = useState("");
+	const [user, setUser] = useState("");
+	const [chatChain, setChatChain] = useState([]);
+	useEffect(() => {
+		if(!socket) {
+			setSocket(io());
+		} else {
+			socket.on('chat-message-update', msg => {
+				setChatChain(chain => {
+					chain.push(msg);
+					if(chain.length > 100) {
+						chain.shift();
+					}
+					return [...chain];
+				});
+			});
+		}
+	}, [socket]);
+
+	const handleUserChange = event => {
+		setUser(event.target.value);
+	}
+	const handleMsgChange = event => {
+		setMessage(event.target.value);
+	}
+	const handleMsgSubmit = event => {
+		event.preventDefault();
+		socket.emit('chat-message-submit', {user: user || "Anomynous", message});
+		setMessage("");
+		return false;
+	}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div className="App">
+			<ul>{chatChain.map((msg, index) => {
+				return <li key={index}>{msg.user}[{msg.ip}]: {msg.message}</li>
+			})}</ul>
+			<form action="">
+				<input 
+					style={{width: '15%'}}
+					placeholder="Anonymous"
+					autoComplete="off" 
+					value={user} 
+					onChange={handleUserChange}
+				/>
+
+				<input 
+					style={{width: '75%'}}
+					placeholder="Message"
+					autoComplete="off" 
+					value={message} 
+					onChange={handleMsgChange}
+				/>
+				<button onClick={handleMsgSubmit}>Send</button>
+			</form>
+		</div>
   );
 }
 
