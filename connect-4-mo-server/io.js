753acf306eda2939b5a44a542d0022ba9264a7d5
rh07 <rhuang07@gmail.com>
2019-12-23 05:37:02 -0800

    add disconnect handling; refactor error handling flow

--- connect-4-mo-server/io.js
+++ connect-4-mo-server/io.js
@@ -8,43 +8,50 @@ function conn_init(socket) {
 	socket.room = 'Lobby';
 	socket.join('Lobby');
 
+	add_user(socket.client.id, socket.room);
+
 	socket.emit('chat-update',
 		{...srvmsg, message:'Connected to lobby.'});
 	socket.broadcast.emit('chat-update',
 		{...srvmsg, message:socket.handshake.address + ' has connected.'});
 }
 
-function add_user(user, room) {
-	var roomusers = rooms.get(room);
-	if (typeof roomusers === 'undefined') {
-		console.log("Inavlid room: " + room + ".");
+function conn_fini(socket) {
+	var user = socket.client.id;
+	if (!users.has(user)) {
+		console.log("Invalid client ID: " + user + ".");
 		return -1;
 	}
 
-	roomusers.add(user);
+	if(del_user(user, socket.room) < 0)
+		console.log("Failed to delete user %s from room %s.",
+			user, socket.room);
+
+	users.delete(user);
+}
+
+function add_user(user, room) {
+	if (!rooms.has(room))
+		return -1;
+
+	rooms.get(room).add(user);
 	users.set(user, room);
 }
 
 function del_user(user, room) {
-	var roomusers = rooms.get(room);
-	if (roomusers === 'undefined') {
-		console.log("Inavlid room: " + room + ".");
+	if (!rooms.has(room))
 		return -1;
-	}
 
-	roomusers.delete(user);
+	rooms.get(room).delete(user);
 }
 
 function add_room(room) {
-	if (rooms.has(room))
-		return -1;
 	rooms.set(room, new Set());
 }
 
 function room_join(socket, user, room) {
-	room_leave(socket, user);
-
-	if (add_user(user, room) < 0)
+	if (add_user(socket.client.id, room) < 0
+		|| room_leave(socket, user) < 0)
 		return -1;
 
 	socket.room = room;
@@ -56,11 +63,16 @@ function room_join(socket, user, room) {
 
 function room_leave(socket, user) {
 	var room = socket.room;
-	del_user(user, room);
+	if(del_user(socket.client.id, room) < 0)
+		return -1;
 
 	socket.leave(room);
 	socket.broadcast.to(room).emit('chat-update',
 		{...srvmsg, message:user + ' has left.'});
+}
+
+function emit_err(socket, cmd, err) {
+	socket.emit(cmd, 'Error: ' + err);
 }
 
 module.exports = function (http, app) {
@@ -72,26 +84,22 @@ module.exports = function (http, app) {
 		socket.on('disconnect', function() {
 			socket.broadcast.emit('chat-update',
 				{...srvmsg, message:socket.handshake.address + ' has disconnected.'});
+			conn_fini(socket);
 		});
 
 		socket.on('room-create', function(data) {
-			if (add_room(data.room) < 0) {
-				socket.emit('ret-err', 'Room ' + data.room + ' already exists.');
-				return -1;
-			}
-			if (room_join(socket, data.user, data.room) < 0) {
-				socket.emit('ret-crit', 'Failed to create room ' + data.room + '.');
-				return -1;
-			}
+			if (add_room(data.room) < 0)
+				emit_err(socket, 'ret-err', 'Room ' + data.room + ' already exists.');
+
+			if (room_join(socket, data.user, data.room) < 0)
+				emit_err(socket, 'ret-crit', 'Cannot create room ' + data.room + '.');
 
 			socket.emit('room-update', socket.room);
 		});
 
 		socket.on('room-join', function(data) {
-			if (room_join(socket, data.user, data.room) < 0) {
-				socket.emit('ret-err', 'Cannot find ' + data.room + '.');
-				return -1;
-			}
+			if (room_join(socket, data.user, data.room) < 0)
+				emit_err(socket, 'ret-err', 'Cannot join room ' + data.room + '.');
 
 			socket.broadcast.to(data.room).emit('chat-update',
 				{...srvmsg, message:data.user + ' has joined.'});
